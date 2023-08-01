9e3234fef7faa3bce0211c3dcab604d0334e08e3
juniobash <juniobash@outlook.com>
2023-07-31 22:54:36 -0300

    criando o primeiro programa em c

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Programando em C
 > `gcc`
+
+~~~bash
+gcc nymph.c -o nymph
+~~~
---
+++ nymph.c
@@ -0,0 +1,9 @@
+/*
+    Estrutura Mínima 
+	de Um programa em C
+*/
+#include <stdio.h>
+
+void main (void) {
+    printf("Hello, cruel world!\n");
+}
